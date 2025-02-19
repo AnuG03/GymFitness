5a0684ac4fbc7bc17bdaf5717902fc4bcbed6242
AnuG03 <anukrishnan259@gmail.com>
2025-02-19 13:02:42 +0530

    Initial Django project commit

--- feeds/admin.py
+++ feeds/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+from feeds.models import Contact,MembershipPlan,Enrollment,Trainer,Gallery,Attendance,Award,Service
 
-# from feeds.models import feeds
-# admin.site.register(feeds)
+admin.site.register(Contact)
+admin.site.register(MembershipPlan)
+admin.site.register(Enrollment)
+admin.site.register(Trainer)
+admin.site.register(Gallery)
+admin.site.register(Attendance)
+admin.site.register(Award)
+admin.site.register(Service)
--- feeds/urls.py
+++ feeds/urls.py
@@ -2,5 +2,22 @@ from django.urls import path
 from feeds import views
 
 urlpatterns = [
-    path('',views.Home,name="Home")
+    path('',views.Home,name="Home"),
+    path('signup',views.signup,name="signup"),
+    path('login',views.handlelogin,name="handlelogin"),
+    path('logout',views.handleLogout,name="handleLogout"),
+    path('contact',views.contact,name="contact"),
+    path('join',views.enroll,name="enroll"),
+    path('profile',views.profile,name="profile"),
+    path('gallery',views.gallery,name="gallery"),
+    path('attendance',views.attendance,name="attendance"),
+    path('award',views.award,name="award"),
+    path('about',views.about,name="about"),
+    path('service',views.service,name="service"),
+    path('bi_tri',views.bi_tri,name="bi_tri"),
+    path('back',views.back,name="back"),
+    path('shoulders',views.shoulders,name="shoulders"),
+    path('chest',views.chest,name="chest"),
+    path('legs',views.legs,name="legs"),
+    path('policy',views.policy,name="policy"),
 ]
